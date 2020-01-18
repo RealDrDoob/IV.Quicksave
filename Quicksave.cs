e093ad1a0ee1f4181872a89e5a1f8f7fab5dded3
RealDrDoob <4370526+RealDrDoob@users.noreply.github.com>
2020-01-17 19:38:47 -0500

    Upload mod.

---
+++ Quicksave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using GTA;
+
+namespace Quicksave {
+   public class Quicksave : Script {
+
+      public Quicksave() {
+          this.KeyDown += new GTA.KeyEventHandler(this.Quicksave_KeyDown);
+      }
+
+      private void Quicksave_KeyDown(object sender, GTA.KeyEventArgs e)
+      {
+          if (e.Key == Settings.GetValueKey("Key", Keys.F5))
+          {
+              Game.DoAutoSave();
+          }
+
+
+          if (e.Key == Keys.F5 && e.Alt)
+          {
+              Game.ShowSaveMenu();
+          }
+      }
+
+   }
+
+}
